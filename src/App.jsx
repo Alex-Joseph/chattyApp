3c3f69af0336b092b92540718b581808d2aa955b
Alex-Joseph <alex.cheng@ryerson.ca>
2017-03-06 22:52:36 +0000

    initial setup of ChattyApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
+    console.log("Rendering <App/>")
     return (
-      <h1>Hello React :)</h1>
-    );
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <Message />
+        <ChatBar />
+      </div>
+
+    )
   }
 }
 export default App;
---
+++ src/MessageList.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    console.log("Rendering <MessageList/>");
+    return <main className="messages"/>
+  }
+}
+export default MessageList;
